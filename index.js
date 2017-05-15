81cc1e72c44d1e2b2bdb82609841a79e966bf34e
Eric Wang <ezwang1@gmail.com>
2017-05-14 22:16:02 -0400

    set up login and faq views

--- index.js
+++ index.js
@@ -2,11 +2,20 @@ var express = require("express");
 var app = express();
 
 app.use(express.static("static"));
+app.engine("html", require("ejs").renderFile);
+
+var login_url = "https://ion.tjhsst.edu/oauth/authorize?client_id=ySHTIuQ6Hmn2oyJXY8nHtJTRU6unUdny00N3nZAL&response_type=token";
 
 app.get("/", function(req, res) {
-    res.sendfile("static/login.html");
+    res.render("login.html", { login_url: login_url });
 });
 
-app.listen(process.env.PORT, function() {
-    console.log("TJTinder listening on port " + process.env.PORT + "!");
+app.get("/faq", function(req, res) {
+    res.render("faq.html");
+});
+
+var port = process.env.PORT;
+
+app.listen(port, function() {
+    console.log("TJTinder listening on port " + port + "!");
 });
